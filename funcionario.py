95f2346e8d6651eed0023b13952c2b7722b00ebd
renan <camposrenanrc@gmail.com>
2019-08-13 17:53:41 -0300

    adicionar atividade do dia

---
+++ funcionario.py
@@ -0,0 +1,25 @@
+dia = int(input('Digite um número de 1 á 7: '))
+
+if dia == 1 :
+    print ('Hoje é Domingo!')
+
+elif dia == 2 :
+    print ('Hoje é Segunda-Feira')
+
+elif dia == 3 :
+    print ('Hoje é terça-Feira')
+
+elif dia == 4 :
+    print ('Hoje é Quarta-Feira')
+
+elif dia == 5 :
+    print ('Hoje é Quinta-Feira')
+
+elif dia == 6 :
+    print ('Hoje é Sexta-Feira')
+
+elif dia == 7 :
+    print ('Hoje é Sabado-Feira')
+
+else:
+    print ('Dia da semana não encontrado')
